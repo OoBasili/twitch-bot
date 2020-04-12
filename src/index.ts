46d53da7c66e0308bc0e574300470301a75bd510
Romanov Vasiliy <vasilok11@gmail.com>
2020-04-12 20:32:08 +0300

    ESLint added

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
 import { Client, Options, ChatUserstate } from 'tmi.js';
 
 const options: Options = {
-  identity: {
-    username: 'basilioobot',
-    password: ''
-  },
-  channels: [
-    'basilioo197',
-  ]
+	identity: {
+		username: 'basilioobot',
+		password: ''
+	},
+	channels: [
+		'basilioo197',
+	]
 };
 
 const client: Client = Client(options);
 
+function onMessageHandler (target: string, context: ChatUserstate, msg: string, self: boolean): void {
+	if (self)
+		return;
+
+	if (context['user-id'] === '???????') {
+		client.say(target, `@${context['display-name']} привет`);
+	}
+}
+
+function onConnectedHandler (addr: string, port: number): void {
+	console.log(`* Connected to ${addr}:${port}`);
+}
+
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
 client.connect();
-
-function onMessageHandler (target: string, context: ChatUserstate, msg: string, self: boolean) {
-  if (self) { return; }
-
-  if (context["user-id"] === '???????') {
-    client.say(target, `@${context["display-name"]} привет`);
-  }
-}
-
-function onConnectedHandler (addr: string, port:number) {
-  console.log(`* Connected to ${addr}:${port}`);
-}
