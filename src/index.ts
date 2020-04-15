ea0b05e22cb6fbe7098389b1c5eec859592b068f
Romanov Vasiliy <vasilok11@gmail.com>
2020-04-15 13:06:01 +0300

    Project settings update

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import { Client, Options, ChatUserstate } from 'tmi.js';
+import { Options, ChatUserstate, Client } from 'tmi.js';
 
 const options: Options = {
-	identity: {
-		username: 'basilioobot',
-		password: ''
-	},
-	channels: [
-		'basilioo197',
-	]
+  identity: {
+    username: 'basilioobot',
+    password: ''
+  },
+  channels: [
+    'basilioo197',
+  ]
 };
 
 const client: Client = Client(options);
 
-function onMessageHandler (target: string, context: ChatUserstate, msg: string, self: boolean): void {
-	if (self)
-		return;
+function onMessageHandler(target: string, context: ChatUserstate, msg: string, self: boolean): void {
+  if (self)
+    return;
 
-	if (context['user-id'] === '???????') {
-		client.say(target, `@${context['display-name']} привет`);
-	}
+  if (context['user-id'] === '???????') {
+    client.say(target, `@${context['display-name']} привет`);
+  }
 }
 
-function onConnectedHandler (addr: string, port: number): void {
-	console.log(`* Connected to ${addr}:${port}`);
+function onConnectedHandler(addr: string, port: number): void {
+  console.log(`* Connected to ${addr}:${port}`);
 }
 
 client.on('message', onMessageHandler);
